801b81a62c946227bd569caa0e86bd85bdfd0bc5
MarkusTryban <markustryban@gmail.com>
2019-12-31 10:15:53 -0500

    Replace path for all bootcamp routes: / & /:id

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -2,28 +2,30 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/api/v1/bootcamps', (req, res) => {
+router.get('/', (req, res) => {
   res.status(200).json({ success: true, msg: 'Show all bootcamps' });
 });
 
-router.get('/api/v1/bootcamps/:id', (req, res) => {
+router.get('/:id', (req, res) => {
   res
     .status(200)
     .json({ success: true, msg: `Show bootcamp ${req.params.id}` });
 });
 
-router.post('/api/v1/bootcamps', (req, res) => {
+router.post('/', (req, res) => {
   res.status(200).json({ success: true, msg: 'Crate new bootcamp' });
 });
 
-router.put('/api/v1/bootcamps/:id', (req, res) => {
+router.put('/:id', (req, res) => {
   res
     .status(200)
     .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
 });
 
-router.delete('/api/v1/bootcamp/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
   res
     .status(200)
     .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
 });
+
+module.exports = router;
