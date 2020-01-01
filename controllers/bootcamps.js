3dde31325ceeff87b564ba0b9d05dae633130d4b
MarkusTryban <markustryban@gmail.com>
2019-12-31 19:09:08 -0500

    Move bootcamp routes to controllers folder

---
+++ controllers/bootcamps.js
@@ -0,0 +1,46 @@
+const express = require('express');
+
+const router = express.Router();
+
+// @desc      Get all bootcamps
+// @route     GET /api/v1/bootcamps
+// @access    Public
+exports.getBootcamps = (req, res, next) => {
+  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+};
+
+// @desc        Get single bootcamp
+// @route       GET /api/v1/bootcamps/:id
+// @access      Public
+exports.getBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Show bootcamp ${req.params.id}` });
+};
+
+// @desc        Create new bootcamp
+// @route       POST /api/v1/bootcamp
+// @access      Private
+exports.createBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true, msg: 'Crate new bootcamp' });
+};
+
+// @desc        Update bootcamp
+// @route       PUT /api/v1/bootcamp/:id
+// @access      Private
+exports.updateBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+};
+
+// @desc        Delete bootcamp
+// @route       DELETE /api/v1/bootcamp/:id
+// @access      Private
+exports.deleteBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+};
+
+module.exports = router;
