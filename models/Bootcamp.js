f841ba9092efc7856de8df3dd1645a25c3da9939
MarkusTryban <markustryban@gmail.com>
2020-01-03 20:19:09 -0500

    Add slug feild to BootcampSchema

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -6,6 +6,12 @@ const BootcampSchema = new mongoose.Schema({
     require: [true, 'Please enter a name'],
     unique: true,
     trim: true,
-    maxlength: [50, 'Name can be no more than 50 char']
+    maxlength: [50, 'Name can be no more than 50 characters']
+  },
+  slug: String,
+  description: {
+    type: String,
+    require: [true, 'Please enter a description'],
+    maxlength: [500, 'Description can be no more than 500 characters']
   }
 });
