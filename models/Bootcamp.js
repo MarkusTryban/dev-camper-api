4cb6636caa9689f7d86a028c9e0e30159bd1869f
MarkusTryban <markustryban@gmail.com>
2020-01-03 20:23:40 -0500

    Add phone & email feilds to BootcampSchema

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -20,5 +20,16 @@ const BootcampSchema = new mongoose.Schema({
       /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
       'Please use a valid URL with HTTP or HTTPS'
     ]
+  },
+  phone: {
+    type: String,
+    maxlength: [20, 'Phone number can not be longer than 20 characters']
+  },
+  email: {
+    type: String,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Please add a valid email'
+    ]
   }
 });
