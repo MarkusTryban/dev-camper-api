b229f25892fbea78b0d13e6084e9a78d32b39b27
MarkusTryban <markustryban@gmail.com>
2020-01-04 18:47:53 -0500

    Create new bootcamp & add it to the database

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -19,8 +19,13 @@ exports.getBootcamp = (req, res, next) => {
 // @desc        Create new bootcamp
 // @route       POST /api/v1/bootcamp
 // @access      Private
-exports.createBootcamp = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Crate new bootcamp' });
+exports.createBootcamp = async (req, res, next) => {
+  const bootcamp = await Bootcamp.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    data: bootcamp
+  });
 };
 
 // @desc        Update bootcamp
