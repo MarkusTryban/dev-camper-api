ffe346347b67f9a7acd9b5468d179a2a0e822dba
MarkusTryban <markustryban@gmail.com>
2020-01-05 18:49:30 -0500

    Throw err if bootcamp id does not exist

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -20,6 +20,10 @@ exports.getBootcamp = async (req, res, next) => {
   try {
     const bootcamp = await Bootcamp.findById(req.params.id);
 
+    if (!bootcamp) {
+      return res.status(400).json({ success: false });
+    }
+
     res.status(200).json({ success: true, data: bootcamp });
   } catch (err) {
     res.status(400).json({ success: false });
