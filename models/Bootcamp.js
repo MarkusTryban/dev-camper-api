ed26a57f68c57c5cb8c28019eeb7620faa9fa174
MarkusTryban <markustryban@gmail.com>
2020-02-01 20:13:00 -0500

    Remove unneeded coments

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -106,13 +106,11 @@ const BootcampSchema = new mongoose.Schema(
   }
 );
 
-// Create bootcamp slug from name
 BootcampSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
 
-// Geocode & create location field
 BootcampSchema.pre('save', async function(next) {
   const loc = await geocoder.geocode(this.address);
   this.location = {
