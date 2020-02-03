4e39d90f8170b587d7ab0549485fc404de89e367
MarkusTryban <markustryban@gmail.com>
2020-02-02 20:04:58 -0500

    Delete all courses when deleting boocamp

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -129,6 +129,11 @@ BootcampSchema.pre('save', async function(next) {
   next();
 });
 
+BootcampSchema.pre('remove', async function(next) {
+  await this.model('Course').deleteMany({ boocamp: this._id });
+  next();
+});
+
 // Reverse populate with virtuals
 BootcampSchema.virtual('courses', {
   ref: 'Course',
